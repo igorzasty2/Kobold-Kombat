2f49ae0806f2413b67e32cccfaaf830951e8ac11
Igor Osiakoski <igorzasty2@wp.pl>
2024-06-11 12:21:29 +0200

    Added Screen Transitions

--- Assets/Code/Scripts/MainMenu.cs
+++ Assets/Code/Scripts/MainMenu.cs
@@ -1,11 +1,20 @@
+using System.Collections;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Animator transition;
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        StartCoroutine(StartGame());
+    }
+    private IEnumerator StartGame()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame()
     {
